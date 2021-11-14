c1857e8eec55b78023fa2c8158b73d79d7bfb568
Jeonsangyeon2304 <wjstkddus01@gmail.com>
2021-11-14 19:23:36 +0900

    Add CalculatorAPI

---
+++ api/src/main/java/com/study/api/FormData.java
@@ -0,0 +1,32 @@
+package com.study.api;
+
+public class FormData {
+    private int nVal1;
+    private int nVal2;
+    private String szOper;
+
+    public int getNVal1() {
+        return this.nVal1;
+    }
+
+    public void setNVal1(int nVal1) {
+        this.nVal1 = nVal1;
+    }
+
+    public int getNVal2() {
+        return this.nVal2;
+    }
+
+    public void setNVal2(int nVal2) {
+        this.nVal2 = nVal2;
+    }
+
+    public String getSzOper() {
+        return this.szOper;
+    }
+
+    public void setSzOper(String szOper) {
+        this.szOper = szOper;
+    }
+    
+}
--- api/src/main/java/com/study/api/RESTApi.java
+++ api/src/main/java/com/study/api/RESTApi.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,47 @@ public class RESTApi {
     @PostMapping("/class")
     public int addUser(@RequestBody User user) {
         return user.getId();
+    }
+
+    @PostMapping("/Calc")
+    public String calcutation(@ModelAttribute FormData formdata) {
+        int nValue1 = formdata.getNVal1();
+        int nValue2 = formdata.getNVal2();
+        String szOper = formdata.getSzOper();
+        String szRet = "";
+
+        szOper = szOper.trim();
+
+        if( szOper.length() == 1 ) {
+            int nResult = 0;
+
+            switch( szOper ) {
+                case "+" :
+                    nResult = nValue1 + nValue2;
+                    szRet = nValue1 + " + " + nValue2 + " = " + nResult;
+                    break;
+                case "-" :
+                    nResult = nValue1 - nValue2;
+                    szRet = nValue1 + " - " + nValue2 + " = " + nResult;
+                    break;
+                case "*" :
+                    nResult = nValue1 * nValue2;
+                    szRet = nValue1 + " * " + nValue2 + " = " + nResult;
+                    break;
+                case "/" :
+                    if( nValue1 != 0 )
+                    {
+                        nResult = nValue1 / nValue2;
+                        szRet = nValue1 + " / " + nValue2 + " = " + nResult;
+                    }
+                    else
+                    szRet = "Can not divide Zero";
+                    break;
+            }
+        }
+        else
+            szRet = "Input Wrong Operation";
+
+        return szRet;
     }
 }
